b046cda1656c5b141994c62c72150828779642cb
Arthur Mosley <arthucharlesmosley@gmail.com>
2025-06-01 21:35:19 -0400

    some clojure related changes

--- personal/config-clojure-mode.el
+++ personal/config-clojure-mode.el
@@ -1,26 +1,11 @@
-;; clojure-lsp integration via lsp-mode
-
 (use-package lsp-mode
-  :ensure t
-  :commands (lsp lsp-deferred)
-  :hook ((clojure-mode        ; for .clj files
-          clojurec-mode       ; for compiled Clojure files
-          clojurescript-mode) ; for .cljs files
-         . lsp-deferred)
   :init
-  ;; Optional: set prefix for lsp-command-keymap (defaults to "C-c l")
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
-  :config
-  ;; Prefer Flycheck over Flymake for diagnostics
-  (setq lsp-prefer-flymake nil)
-  ;; Adjust clojure-lsp server command if necessary.
-  (setq lsp-clojure-server-command '("clojure-lsp")))
+  :hook ((clojure-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
-(use-package lsp-ui
-  :ensure t
-  :commands lsp-ui-mode
-  :config
-  (setq lsp-ui-sideline-enable t
-        lsp-ui-doc-enable t))
-
-;; You can also customize any additional lsp settings as desired.
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
